4516b892c2ee36da6b0e6599eac4b5fd03272b8e
Keshava Reddy.S <146667614+Keshava199-web@users.noreply.github.com>
2026-02-27 21:37:01 +0530

    Enhance WiFi password retrieval functionality
    Retrieve and store WiFi profiles and their passwords.

--- Wifi-passwd_Cr4ck3r.py
+++ Wifi-passwd_Cr4ck3r.py
@@ -14,4 +14,32 @@ wifi_list = list()
 
 if len(profile_names) != 0:
   for name in profile_names:
+    wifi_profile = {}
+    profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+        #    We use the regular expression to only look for the absent cases so we can ignore them.
+        if re.search("Security key           : Absent", profile_info_pass):
+            continue
+        else:
+            #    Assign the ssid of the wifi profile to the dictionary.
+            #wifi_profile["ssid"] = name
+            wifi_profile = {"ssid" : name, "password" : None}
+            #    These cases aren't absent and we should run the 
+            #    "key=clear" command part to get the password.
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+            #    Again run the regular expression to capture the 
+            #    group after the : (which is the password).
+            password = re.search("Key Content          : (.*)\r", profile_info_pass)
+            #    Check if we found a password using the regular expression. 
+            #    Some wifi connections may not have passwords.
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+                #    We assign the grouping (where the password is contained) that 
+                #    we are interested in to the password key in the dictionary.
+                wifi_profile["password"] = password.group[1]
+            #    We append the wifi information to the variable wifi_list.
+            wifi_list.append(wifi_profile) 
+
+for x in range(len(wifi_list)):
+    print(wifi_list[x]) 
     
